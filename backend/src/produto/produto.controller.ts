a7e705cb9311d9818c000583a9cca52a8627a266
Turbobot <turbobot@vercel.com>
2024-10-05 08:42:35 -0300

    feat(create-turbo): apply official-starter transform

--- backend/src/produto/produto.controller.ts
+++ backend/src/produto/produto.controller.ts
@@ -1,19 +1,32 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { Produto, produtos } from 'src/core';
+import { ProdutoPrisma } from './produto.prisma';
 
 @Controller('produtos')
 export class ProdutoController {
+
+    constructor(readonly repo: ProdutoPrisma) {
+
+    }
+
+    @Post()
+    salvarProduto(@Body() produto: Produto): Promise<void> {
+        return this.repo.salvar(produto)
+    }
+
     @Get()
-    obterProdutos(): Produto[] {
-        return produtos.map((produto) => ({
-            ...produto,
-            especificacoes: {destaque: produto.especificacoes.destaque}
-        }))
+    obterProdutos(): Promise<Produto[]> {
+        return this.repo.obter()
     }
 
     @Get(':id')
     async obterProdutoPorId(@Param('id') id: string): Promise<Produto | null>{
-        const produto = produtos.find((produto) => produto.id === +id)
-        return produto ? produto : null
+        return this.repo.obterPorId(+id)
     }
+
+    @Delete(':id')
+    async excluirProduto(@Param('id') id: string): Promise<void> {
+        return this.repo.excluir(+id)
+    }
+
 }
